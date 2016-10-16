f8bb86f2cffd927db836afec5c16052af3b173c4
Mike Dacre <mike.dacre@gmail.com>
2016-10-15 23:47:55 -0700

    Added collapse_dataframe function

--- grasp/__init__.py
+++ grasp/__init__.py
@@ -1,6 +1,7 @@
 """
 A Simple GRASP (grasp.nhlbi.nih.gov) API based on SQLAlchemy and Pandas
 """
+
 # Version Number
 __version__ = '0.1.4b'
 
@@ -11,6 +12,7 @@ from . import query
 from . import tables
 from .db import get_session
 from .tables import SNP, Phenotype, PhenoCats, Platform, Population
+from .query import get_studies, get_snps
 
-__all__ = ["tables", "query", "get_session"]
+__all__ = ["tables", "query", "get_session", "get_studies", "get_snps"]
 
--- grasp/query.py
+++ grasp/query.py
@@ -23,7 +23,7 @@ __all__ = ["get_studies", "get_snps", "get_variant_info", "get_pop_flags",
 def get_studies(primary_phenotype=None, pheno_cats=None, pheno_cats_alias=None,
                 primary_pop=None, has_disc_pop=None, has_rep_pop=None,
                 only_disc_pop=None, only_rep_pop=None, query=False,
-                count=False, pandas=False):
+                count=False, dictionary=False, pandas=False):
     """Return a list of studies filtered by phenotype and population.
 
     There are two ways to query both phenotype and population.
@@ -95,6 +95,7 @@ def get_studies(primary_phenotype=None, pheno_cats=None, pheno_cats_alias=None,
         query:             Return the query instead of the list of study
                            objects.
         count:             Return a count of the number of studies.
+        dictionary:        Return a dictionary of title->id for filtering.
         pandas:            Return a dataframe of study information instead
                            of the list.
 
@@ -174,6 +175,8 @@ def get_studies(primary_phenotype=None, pheno_cats=None, pheno_cats_alias=None,
         return q
     elif count:
         return q.count()
+    elif dictionary:
+        return {i.title: i.id for i in q.all()}
     elif pandas:
         return _pd.read_sql(q.statement, e)
     else:
@@ -189,8 +192,13 @@ def get_snps(studies, pandas=True):
 
     """
     s, e = _db.get_session()
+    if isinstance(studies, dict):
+        studies = list(studies.values())
+    if not isinstance(studies, (list, tuple)):
+        studies = [studies]
     if isinstance(studies[0], t.Study):
         studies = [i.id for i in studies]
+    studies = list(studies)
 
     if pandas:
         dfs = []
@@ -213,9 +221,194 @@ def get_snps(studies, pandas=True):
             ).all()
         return snps
 
+
+def find_intersecting_phenotypes(primary_pops=None, pop_flags=None,
+                                 check='cat', pop_type='disc',
+                                 exclusive=False, list_only=False):
+    """Return a list of phenotypes that are present in all populations.
+
+    Can only provide one of primary_pops or pop_flags. pop_flags does a
+    bitwise lookup, primary_pops quries the primary string only.
+
+    By default this function returns a list of phenotype categories, if you
+    want to check primary phenotypes instead, provide check='primary'.
+
+    Args:
+        primary_pops: A string or list of strings corresponing to the
+                      `tables.Study.phenotype` column
+        pop_flags:    A `ref.PopFlag` object or list of objects.
+        check:        cat/primary either check categories or primary phenos.
+        pop_type:     disc/rep Use with pop_flags only, check either
+                      discovery or replication populations.
+        exclusive:    Use with pop_flags only, do an excusive rather than
+                      inclusion population search
+        list_only:    Return a list of names only, rather than a list of
+                      objects
+
+    Returns:
+        A list of `table.Phenotype` or `table.PhenoCat` objects, or a list of
+        names if `list_only` is specified.
+
+    """
+    # Check arguments
+    if primary_pops and pop_flags:
+        raise KeywordError("Cannot specify both 'primary_pops' and " +
+                           "'pop_flags'")
+    if not primary_pops and not pop_flags:
+        raise KeywordError("Must provide at least one of 'primary_pops' or " +
+                           "'pop_flags'")
+    if check not in ['cat', 'primary']:
+        raise KeywordError("'check' must be one of ['cat', 'primary']")
+
+    # Pick query type
+    if pop_flags:
+        if pop_type not in ['disc', 'rep']:
+            raise KeywordError("'pop_type' must be one of ['disc', 'rep']")
+        l = 'only' if exclusive else 'has'
+        key = '{}_{}_pop'.format(l, pop_type)
+        qpops = [get_pop_flags(i) for i in pop_flags]
+    else:
+        key = 'primary_pop'
+        qpops = primary_pops
+
+    # Check that we have an iterable
+    if not isinstance(qpops, (list, tuple)):
+        raise KeywordError('Population query must be a list or tuple')
+
+    # Get phenotype lists and intersect to form a final set
+    # We use IDs here because it makes the set intersection more robust
+    final_set = set()
+    for pop in qpops:
+        p = []
+        for i in get_studies(**{key: pop}):
+            p += i.phenotype_cats if check == 'cat' else i.phenotype
+        out = set([i.id for i in p])
+        if not final_set:
+            final_set  = out
+        else:
+            final_set &= out
+
+    # Get the final phenotype list
+    s, _ = _db.get_session()
+    phenos = []
+    for id_list in _chunks(list(final_set), 999):
+        table = t.Phenotype if primary_pops else t.PhenoCats
+        phenos += s.query(table).filter(table.id.in_(id_list)).all()
+
+    # Return the list
+    if list_only:
+        return [i.category for i in phenos] if check == 'cat' \
+            else [i.phenotype for i in phenos]
+    else:
+        return phenos
+
+
+###############################################################################
+#                           DataFrame Manipulations                           #
+###############################################################################
+
+
+def collapse_dataframe(df, mechanism='median', pvalue_filter=None,
+                       protected_columns=None):
+    """Collapse a dataframe by chrom:location from get_snps.
+
+    Will use the mechanism defined by 'mechanism' to collapse a dataframe
+    to one indexed by 'chrom:loc' with pvalue and count only.
+
+    This function is agnostic to all dataframe columns other than::
+
+        ['chrom', 'pos', 'snpid', 'pval']
+
+    All other columns are collapsed into a comma separated list, a string.
+    'chrom' and 'pos' are merged to become the new colon-separated index,
+    snpid is maintained, and pval is merged using the function in 'mechanism'.
+
+    Args:
+        df:                A pandas dataframe, must have 'chrom', 'pos',
+                           'snpid', and 'pval' columns.
+        mechanism:         A numpy statistical function to use to collapse the
+                           pvalue, median or mean are the common ones.
+        pvalue_filter:     After collapsing the dataframe, filte to only
+                           include pvalues less than this cutoff.
+        protected_columns: A list of column names that will be maintened as is,
+                           although all duplicates will be dropped (randomly).
+                           Only makes sense for columns that are identical
+                           for all studies of the same SNP.
+
+    Returns:
+        DataFrame: Indexed by chr:pos, contains flattened pvalue column, and
+                   all original columns as a comma-separated list. Additionally
+                   contains a count and stddev (of pvalues) column. stddev is
+                   nan if count is 1.
+
+    """
+    # Copy the dataframe to avoid clobbering it
+    df = df.sort_values(['chrom', 'pos'])
+
+    # Add the location column, which will become the index
+    df['loc'] = df.apply(_create_loc, axis=1)
+
+    # Move all of the columns we want to maintain to a separate df and drop
+    # dups
+    if protected_columns:
+        prot_cols = []
+        for i in protected_columns:
+            if i not in ['loc', 'snpid', 'chrom', 'pos', 'pval']:
+                prot_cols.append(i)
+        df_prot = df[prot_cols]
+        df_prot = df_prot.drop_duplicates('loc')
+        df_prot.set_index('loc', inplace=True)
+        df.drop(prot_cols, axis=1, inplace=True)
+
+    # Protect SNPID, chrom, and loc
+    df_snp = df[['loc', 'snpid', 'chrom', 'pos']]
+    df_snp = df_snp.drop_duplicates('loc')
+    df_snp.set_index('loc', inplace=True)
+    df.drop(['snpid', 'chrom', 'pos'], axis=1, inplace=True)
+
+    # Flatten the pvalue
+    df_p = df[['loc', 'pval']]
+    df.drop('pval', axis=1, inplace=True)
+    df_p = df_p.groupby('loc').aggregate([mechanism, 'std', 'count'])
+    df_p.columns = ['pval_{}'.format(mechanism), 'std', 'count']
+
+    # Flatten the remainder
+    df = df.groupby('loc').aggregate(_aggregate_strings)
+
+    # Recombine
+    comb_list = [df_snp, df_p, df]
+    if protected_columns:
+        comb_list.insert(2, df_prot)
+
+    df = _pd.concat(comb_list, axis=1)
+
+    # Filter
+    if pvalue_filter:
+        df = df[df.pval < pvalue_filter]
+
+    # Done
+    return df
+
+
+def plot_overlapping_snps(series1, series2):
+    """Plot all SNPs that overlap between two pvalue series."""
+    pass
+
+
 ###############################################################################
 #                              Helper Functions                               #
 ###############################################################################
+
+
+def _aggregate_strings(x):
+    """Converts records to comma separated string."""
+    return ','.join(set([str(i) for i in x]))
+
+
+def _create_loc(x):
+    """Merge chrom and pos columns into 'chrom:pos'."""
+    return '{}:{}'.format(x.chrom, x.pos)
+
 
 def get_pop_flags(pop_flags):
     """Merge a list, string, int, or PopFlag series."""
