d1d70c9fcb2c437d4a079864e84f331be72e856f
Mike Dacre <mike.dacre@gmail.com>
2016-10-17 17:50:54 -0700

    Added import checks to __init__

--- grasp/__init__.py
+++ grasp/__init__.py
@@ -5,12 +5,25 @@ A Simple GRASP (grasp.nhlbi.nih.gov) API based on SQLAlchemy and Pandas
 # Version Number
 __version__ = '0.3.0b2'
 
+# Make sure we can import the important stuff
+# We don't check for modules required for initialization here
+import tabulate as _
+import pandas as _
+import sqlalchemy as _
+import myvariant as _
+import flags as _flags
+if not isinstance(_flags.Flags, _flags.FlatMeta):
+    raise ImportError('Could not import py-flags. Make sure py-flags is ' +
+                      'installed instead of pyflags (the hyphen is important)')
+
+
 # Import ourself
 from . import db
 from . import ref
 from . import query
 from . import tables
 from .db import get_session
+from .ref import PopFlag
 from .tables import SNP, Phenotype, PhenoCats, Platform, Population
 from .query import get_studies, get_snps
 
@@ -19,5 +32,5 @@ q = query
 
 __all__ = ['t', 'q', "tables", "query", "get_session", "get_studies",
            "get_snps", "SNP", "Phenotype", "PhenoCats", "Platform",
-           "Population"]
+           "Population", "PopFlag"]
 
